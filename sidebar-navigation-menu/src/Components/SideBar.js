8a02a333375cce1acddb14c2c01aeacb2a57a7a0
mizanurhasan0 <sajal596@diu.edu.bd>
2022-01-11 23:13:52 +0600

    left side navbar design

---
+++ sidebar-navigation-menu/src/Components/SideBar.js
@@ -0,0 +1,26 @@
+import "../App.css";
+import { SidebarData } from "./SidebarData";
+export const SideBar = () => {
+  return (
+    <div className="Sidebar">
+      <h3 className="title">Software-BD</h3>
+      <ul className="SidebarList">
+        {SidebarData.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              id={window.location.pathname == val.link ? "active" : ""}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+            >
+              <div id="icon">{val.icon}</div>
+              <div id="title">{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
